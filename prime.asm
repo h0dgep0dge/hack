b62b964b67d7c2461138d77f796e155d6bbb2390
Rob <rob.t.mckennie@gmail.com>
2025-02-03 02:47:58 +1300

    finished primality test

--- prime.asm
+++ prime.asm
@@ -1,59 +1,207 @@
 @MAIN
 0;JMP
 
-(MULT)
+(MULT) // Computers R2*R3, placing the product in R1, and jumps to R0
+    @count$MULT
+    M=0
+    @product$MULT
+    M=0
+
+    (MULTLOOP)
+        @count$MULT
+        D=M
+        @R2
+        D=M-D
+        @MULTBREAK
+        D;JLE // Break loop if count >= R2
+
+        @count$MULT
+        M=M+1 // Increment count
+
+        @R3
+        D=M
+        @product$MULT
+        M=D+M // Add R3 to the product
+
+    @MULTLOOP
+    0;JMP
+
+    (MULTBREAK)
+
+    @product$MULT
+    D=M
+    @R1
+    M=D // Store product at R1
+    @R0
+    A=M
+    0;JMP // Jump back to main
+
+(DIV) // Computes R3/R4, placing the result in R1 and the remainder in R2, and jumps to R0
+    @count$DIV
+    M=0
+    @R3
+    D=M
+    @dividend$DIV
+    M=D
+
+    (DIVLOOP)
+    
+        @dividend$DIV
+        D=M
+        @R4
+        A=M
+        D=D-A
+        @DIVBREAK
+        D;JLT
+
+        @dividend$DIV
+        M=D
+        @count$DIV
+        M=M+1
+
+    @DIVLOOP
+    0;JMP
+
+    (DIVBREAK)
+
+    @count$DIV
+    D=M
+    @R1
+    M=D
+    @dividend$DIV
+    D=M
+    @R2
+    M=D
+    @R0
+    A=M
+    0;JMP
+
+(ISPRIME) // Computes if R2 is prime, setting R1 to 1 if so, and jumps to R0
+    @R2
+    D=M
+    @test$ISPRIME
+    M=D
+
+    @R0
+    D=M
+    @return$ISPRIME
+    M=D
 
 
-@count
-M=0
-@product
-M=0
+    @test$ISPRIME
+    D=M-1
+    @FALSE$ISPRIME
+    D;JLE // 1 or less is not prime
 
-(MULTLOOP)
-@count
-D=M
-@R1
-D=M-D
-@MULTBREAK
-D;JLE
+    @test$ISPRIME
+    D=M
+    @2
+    D=D-A
+    @TRUE$ISPRIME
+    D;JEQ // 2 is prime
 
-@count
-M=M+1
+    @2
+    D=A
+    @counter$ISPRIME
+    M=D // starting the counter at 2
 
-@R2
-D=M
-@product
-M=D+M
+    (LOOP$ISPRIME)
 
-@MULTLOOP
-0;JMP
+    @counter$ISPRIME
+    D=M
+    @PRINT
+    M=D
 
-(MULTBREAK)
 
-@product
-D=M
-@R0
-M=D
-@CONTINUE
-0;JMP
+    @test$ISPRIME
+    D=M
+    @R3
+    M=D
+
+    @counter$ISPRIME
+    D=M
+    @R4
+    M=D
+
+    @ISPRIME$1
+    D=A
+    @R0
+    M=D
+
+    @DIV
+    0;JMP
+
+    (ISPRIME$1)
+
+    @R2
+    D=M
+    @FALSE$ISPRIME
+    D;JEQ
+
+
+    @counter$ISPRIME // Increment the divisor
+    M=M+1
+
+    @counter$ISPRIME
+    D=M
+    @R2
+    M=D
+    @R3
+    M=D
+    @ISPRIME$2
+    D=A
+    @R0
+    M=D
+
+    @MULT
+    0;JMP
+
+    (ISPRIME$2)
+
+    @R1
+    D=M
+    @test$ISPRIME
+    D=D-M
+    @TRUE$ISPRIME
+    D;JGT
+
+    @LOOP$ISPRIME
+    0;JMP
+
+    (TRUE$ISPRIME)
+        @R1
+        M=1
+        @return$ISPRIME
+        A=M
+        0;JMP
+    (FALSE$ISPRIME)
+        @counter$ISPRIME
+        D=M
+        @PRINT
+        M=D
+        @R1
+        M=0
+        @return$ISPRIME
+        A=M
+        0;JMP
 
 
 (MAIN)
-@123
-D=A
-@R1
-M=D
-
-@99
+@19997
 D=A
 @R2
 M=D
 
-@MULT
+@MAIN$1
+D=A
+@R0
+M=D
+
+@ISPRIME
 0;JMP
 
-(CONTINUE)
-@R0
+(MAIN$1)
+@R1
 D=M
 @PRINT
 M=D
